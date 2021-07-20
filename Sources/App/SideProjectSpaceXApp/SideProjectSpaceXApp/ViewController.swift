8f5b25cdcd6e44adb6638ba1c95c2933a6ce7d82
Christian Slanzi <christian.slanzi@gmail.com>
2021-07-20 20:51:28 +0200

    created app project

---
+++ Sources/App/SideProjectSpaceXApp/SideProjectSpaceXApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideProjectSpaceXApp
+//
+//  Created by Christian Slanzi on 20.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
